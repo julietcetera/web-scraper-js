e6fd71e23866071aeeb1698b9e6897c27e8d3145
julietcetera <juliette.opdenplatz@sti2.at>
2019-08-29 17:27:16 +0200

    Preprocess parameters

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ const {
     JSDOM
 } = jsdom;
 
+/*
+ * TODO: parameter validations
+ * TODO: get rid of "singleton and collection"
+ *   -> just always put everything in an array
+ */
 exports.scrape = async (params) => {
+
+    const {
+        url,
+        tags
+    } = params ;
 
     let body = await request(params.url);
 
@@ -19,38 +29,37 @@ exports.scrape = async (params) => {
     let sample = {};
 
     /* set of items where a quantity of one is expected */
-    const singletonTags = params.tags.singleton; 
+    if (tags.singleton) {
 
-    Object.keys(singletonTags.text).forEach(tag => {
+        Object.keys(tags.singleton.text).forEach(tag => {
 
-        sample[tag] = $(singletonTags.text[tag]).text();
-    });
+            sample[tag] = $(tags.singleton.text[tag]).text();
+        });
 
-    Object.keys(singletonTags.attribute).forEach(tag => {
+        Object.keys(tags.singleton.attribute).forEach(tag => {
 
-        let query = singletonTags.attribute[tag];
-        sample[tag] = $(query[0]).attr(query[1]);
-    });
+            let query = tags.singleton.attribute[tag];
+            sample[tag] = $(query[0]).attr(query[1]);
+        });
+    }
 
 
     /* set of items where a quantity of >1 is possible */
-    const collectionTags = params.tags.collection;
-
-    Object.keys(collectionTags.text).forEach(tag => {
+    Object.keys(tags.collection.text).forEach(tag => {
 
         sample[tag] = [];
 
-        $(collectionTags.text[tag]).each((index, item) => {
+        $(tags.collection.text[tag]).each(function(index, item) {
             sample[tag].push(item.text);
         });
     });
 
-    Object.keys(collectionTags.attribute).forEach(tag => {
+    Object.keys(tags.collection.attribute).forEach(tag => {
 
         sample[tag] = [];
 
-        let query = collectionTags.attribute[tag];
-        $(query[0]).each(function () { // no syntactic sugar here (won't work)!
+        let query = tags.collection.attribute[tag];
+        $(query[0]).each(function() { // no syntactic sugar here (won't work)!
             sample[tag].push($(this).attr(query[1]));
         });
     });
